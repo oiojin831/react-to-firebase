492212e7a0aec421c5c8bb8bbcd7819cca61954d
Eung Jin Lee <oiojin831@gmail.com>
2021-05-30 14:34:45 +0900

    state를 이용하기위해 useState를 사용, data가 null이면 아무것도 표시안됨

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  const goods = [
-    {
-      name: "덩크",
-      category: "신발",
-      position: "신발장",
-      price: 300000,
-    },
-    {
-      name: "응티셔츠",
-      category: "cloth",
-      position: "box1",
-      price: 60000,
-    },
-    {
-      name: "똥싼바지",
-      category: "바지",
-      position: "box1",
-      price: 30000,
-    },
-  ];
+  const [data, setData] = useState();
 
-  return <div>{goods.map(GoodComponent)}</div>;
+  return <div>{data?.map(GoodComponent)}</div>;
 }
+
+const goods = [
+  {
+    name: "덩크",
+    category: "신발",
+    position: "신발장",
+    price: 300000,
+  },
+  {
+    name: "응티셔츠",
+    category: "cloth",
+    position: "box1",
+    price: 60000,
+  },
+  {
+    name: "똥싼바지",
+    category: "바지",
+    position: "box1",
+    price: 30000,
+  },
+];
 
 // prop에 goods array에 있는 하나의 element가 들어왔을때 => <div>덩크 - 신발 - 신발장 - 300000</div> => 이런 컴포넌트를 만들것이다
 function GoodComponent(props) {
