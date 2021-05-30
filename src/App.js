af829f671f11f6101d504d45e50f98a327cdfbd4
Eung Jin Lee <oiojin831@gmail.com>
2021-05-30 14:27:33 +0900

    js object로 데이터 추가

--- src/App.js
+++ src/App.js
@@ -7,3 +7,24 @@ export default function App() {
     </div>
   );
 }
+
+const goods = [
+  {
+    name: "덩크",
+    category: "신발",
+    postion: "신발장",
+    price: 300000,
+  },
+  {
+    name: "응티셔츠",
+    category: "cloth",
+    postion: "box1",
+    price: 60000,
+  },
+  {
+    name: "똥싼바지",
+    category: "바지",
+    postion: "box1",
+    price: 30000,
+  },
+];
