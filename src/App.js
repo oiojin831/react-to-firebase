72622024fb86faf6d198a2676f6687f9e12cf380
Eung Jin Lee <oiojin831@gmail.com>
2021-05-30 14:23:20 +0900

    데이터 html에 직접넣기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
 export default function App() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <div>덩크 - 신발 - 신발장 - 300000</div>
+      <div>응티셔츠 - cloth - box1 - 60000</div>
+      <div>똥싼바지 - 바지 - box1 - 30000</div>
+    </div>
+  );
 }
